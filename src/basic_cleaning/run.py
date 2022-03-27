bcb1040c848526d04826f757b68ce88f9deb0eba
Rodrigo Franco <rodrigoamfranco@gmail.com>
2022-03-27 17:31:32 -0300

    Removed latitude and longitude entries without proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,10 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
+    logger.info("Drop rows in the dataset that are not in the proper geolocation")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Updating max value of minimum_nights")
     df["minimum_nights"] = np.where(df["minimum_nights"] > args.max_nights,
                                     args.max_nights,
